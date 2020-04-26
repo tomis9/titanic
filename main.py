e66f95e3063a5025e165e486db64ff0baea71077
tomis9 <tomis9@buziaczek.pl>
2020-04-26 14:28:39 +0200

    final submission

--- main.py
+++ main.py
@@ -7,26 +7,26 @@ from sklearn.metrics import accuracy_score
 
 train_raw = pd.read_csv(Path("./data/train.csv"))
 
-y = train_raw['Survived']
+y = train_raw["Survived"]
 
-mean_age = train_raw['Age'].mean()
+mean_age = train_raw["Age"].mean()
+
 
 def process_data(data, mean_age=mean_age):
     data = data[["Pclass", "Sex", "Age", "SibSp", "Parch"]]
     data.columns = [col.lower() for col in data.columns]
-    data.rename(
-        {"pclass": "class", "sibsp": "sib"}, axis=1, inplace=True
-    )
-    data.loc[data['age'].isna(), "age"] = mean_age
-    data['is_male'] = (data['sex'] == 'male').astype(int)
-    data.drop('sex', axis=1, inplace=True)
+    data.rename({"pclass": "class", "sibsp": "sib"}, axis=1, inplace=True)
+    data.loc[data["age"].isna(), "age"] = mean_age
+    data["is_male"] = (data["sex"] == "male").astype(int)
+    data.drop("sex", axis=1, inplace=True)
     return data
 
 
 X = process_data(train_raw)
 
 X_train, X_valid, y_train, y_valid = train_test_split(
-    X, y, test_size=0.33, random_state=42)
+    X, y, test_size=0.33, random_state=42
+)
 
 dtc = DecisionTreeClassifier(max_depth=3)
 dtc.fit(X_train, y_train)
@@ -35,4 +35,6 @@ test_raw = pd.read_csv(Path("./data/test.csv"))
 
 test = process_data(test_raw)
 
-dtc.predict(test)
+submission = test_raw[["PassengerId"]]
+submission["Survived"] = dtc.predict(test)
+submission.to_csv(Path("./data/submission.csv"), index=False)
