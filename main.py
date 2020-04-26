4ee80d4b557bd05d0cd20f3854eb6592489fac78
tomis9 <tomis9@buziaczek.pl>
2020-04-26 13:38:22 +0200

    minor hyperparameter tuning, validation set

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pandas as pd
 from pathlib import Path
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
 
 train_raw = pd.read_csv(Path("./data/train.csv"))
 
-y_train = train_raw['Survived']
+y = train_raw['Survived']
 
 mean_age = train_raw['Age'].mean()
 
@@ -19,12 +23,12 @@ def process_data(data, mean_age=mean_age):
     return data
 
 
-X_train = process_data(train_raw)
+X = process_data(train_raw)
 
+X_train, X_valid, y_train, y_valid = train_test_split(
+    X, y, test_size=0.33, random_state=42)
 
-from sklearn.tree import DecisionTreeClassifier
-
-dtc = DecisionTreeClassifier()
+dtc = DecisionTreeClassifier(max_depth=3)
 dtc.fit(X_train, y_train)
 
 test_raw = pd.read_csv(Path("./data/test.csv"))
